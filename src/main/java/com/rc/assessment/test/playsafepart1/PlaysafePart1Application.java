4783fe0c650f014471948f462547292745affec5
Rubatsiro Chiripa <rubatsirochiripa@gmail.com>
2020-08-23 22:27:38 +0200

    first commit

---
+++ src/main/java/com/rc/assessment/test/playsafepart1/PlaysafePart1Application.java
@@ -0,0 +1,13 @@
+package com.rc.assessment.test.playsafepart1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaysafePart1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaysafePart1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/rc/assessment/test/playsafepart1/PlaysafePart1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rc.assessment.test.playsafepart1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaysafePart1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
